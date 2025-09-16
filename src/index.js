4016cba1d1a67d35c10e9137cb097617de087a74
Danial <dmuratov.kz@gmail.com>
2025-09-16 04:21:28 -0400

    feat: configure deployment using gh-pages
    - Added `gh-pages` to dependencies for deployment support.
    - Configured deployment scripts (`predeploy` and `deploy`) in `package.json`.
    - Updated `BrowserRouter` to use `basename` for proper routing.
    - Set up the homepage URL in `package.json` for GitHub Pages hosting.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./styles/globals.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename="/framelab-ui">
             <App />
         </BrowserRouter>
     </Provider>
